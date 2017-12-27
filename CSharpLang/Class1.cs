f579ffb15722b557b2c1b54cef739aaa8370b33f
Jason Hess <jasonehess@hotmail.com>
2017-12-27 16:30:23 +1000

    Add inline out variable declaration

---
+++ CSharpLang/Class1.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace CSharpLang
+{
+    public class Class1
+    {
+        [Test]
+        public void CanInlineDeclareOutParameterVariables()
+        {
+            // prior to C# 7.0
+            int result;
+            if (int.TryParse("1", out result))
+            {
+                
+            }
+
+            // in C# 7.0
+            if (int.TryParse("1", out int theResult))
+            {
+                
+            }
+        }
+    }
+}
