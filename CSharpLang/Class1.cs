63827755f87d5c85901d8893de3420209122bfa1
Jason Hess <jasonehess@hotmail.com>
2017-12-27 16:51:08 +1000

    Add Tuple support

--- CSharpLang/Class1.cs
+++ CSharpLang/Class1.cs
@@ -34,5 +34,32 @@ namespace CSharpLang
             Console.WriteLine(aResult);
 
         }
+
+        /// <summary>
+        /// Tuple names can now be specified.  Prior to C# 7.0 Tuples existed but their 
+        /// members could only be accessed through Item1, Item2, and so on.  Tuple.Create
+        /// is no longer needed.
+        /// 
+        /// Tuples are useful when you need a new type, but don't need to go to the effort
+        /// of creating a new `class` or `struct`.
+        /// 
+        /// You must include the System.ValueTuple NuGet package on platforms that do not
+        /// include it.  This is because the .NET Standard and C# rev independently of each other.
+        /// </summary>
+        [Test]
+        public void LanguageSupportForTuples()
+        {
+            // prior to C# 7.0
+            var firstTuple = new Tuple<int, int>(1, 2);
+            var secondTuple = Tuple.Create(1, 2);
+            var value = secondTuple.Item2 + secondTuple.Item1;
+
+            // In C# 7.0 Tuples are now understood in the language:
+            var x = (1, 2);
+            x.Item1++;
+            x.Item2++;
+
+
+        }
     }
 }
