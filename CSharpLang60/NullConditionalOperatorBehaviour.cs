55d32c3b9b0bdd97396ef767b229afb0765d90da
Jason Hess <jasonehess@hotmail.com>
2018-01-02 18:48:08 +1000

    Adding C# 6.0 behaviour

--- CSharpLang60/NullConditionalOperatorBehaviour.cs
+++ CSharpLang60/NullConditionalOperatorBehaviour.cs
@@ -1,7 +1,45 @@
-﻿namespace CSharpLang60
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace CSharpLang60
 {
+    /// <summary>
+    /// C# 6.0 adds the null conditional operator `?.` which short-circuits and 
+    /// returns null if its LHS argument is null.
+    /// </summary>
     public class NullConditionalOperatorBehaviour
     {
-        
+        [Test]
+        [TestCase(null, null)]
+        [TestCase("bob", 3)]
+        public void ShouldReturnNullWhenOperatingOnNull(string underTest, int expectedLength)
+        {
+            // If `underTest` is `null`, then `?.` should short-circuit and return null
+            underTest?.Length.Should().Be(expectedLength);
+        }
+
+
+        [Test]
+        [TestCase(null, null)]
+        [TestCase("bob", 3)]
+        public void ShouldReturnTypeOfFullExpressionEvenWhenNull(string underTest, int expectedLength)
+        {
+            // If `underTest` is `null`, then `?.` should short-circuit and return null
+            underTest?.Length.GetType().Should().Be(typeof(int));
+        }
+    }
+
+    /// <summary>
+    /// `string`s can now be composed without need for the `+` operator
+    /// </summary>
+    public class StringInterpolationBehaviour
+    {
+        [Test]
+        public void ShouldInterpolate()
+        {
+            int x = 1;
+            int y = 2;
+            $"x: {x} y: {y}".Should().Be("x: 1 y: 2");
+        }
     }
 }
