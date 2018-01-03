75fe750cf92c9231c5c3040db6b3988c6f8fe005
Jason Hess <jasonehess@hotmail.com>
2018-01-03 17:29:43 +1000

    Add C# nameof operator keyword

---
+++ CSharpLang60/ExceptionFilterBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CSharpLang60
+{
+    public class ExceptionFilterBehaviour
+    {
+        [Test]
+        public void RethrowLosesStackTrace()
+        {
+            Action x = () =>
+            {
+                int erroCount = 0;
+                try
+                {
+                    Throws();
+                }
+                catch (Exception ex)
+                {
+                    erroCount++;
+                    throw;
+                }
+                finally
+                {
+                    erroCount.Should().Be(1);
+                }
+            };
+            var stackTrace = x.ShouldThrow<Exception>().And;
+        }
+
+        public void Throws()
+        {
+            throw new Exception();
+        }
+    }
+}
---
+++ CSharpLang60/NameOfBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace CSharpLang60
+{
+    /// <summary>
+    /// C# 6.0 now allows you to retrieve the name of an identifier with the `nameof` 
+    /// operator keyword
+    /// </summary>
+    public class NameOfBehaviour
+    {
+        [Test]
+        public void ShouldReturnNonFullyQualifiedNameOfIdentifier()
+        {
+            nameof(ShouldReturnNonFullyQualifiedNameOfIdentifier).Should().Be("ShouldReturnNonFullyQualifiedNameOfIdentifier");
+            nameof(NameOfBehaviour).Should().Be("NameOfBehaviour");
+            nameof(Test).Should().Be("Test");
+            nameof(CSharpLang60).Should().Be("CSharpLang60");
+            var someVariable = 10;
+            nameof(someVariable).Should().Be("someVariable");
+        }
+    }
+}
