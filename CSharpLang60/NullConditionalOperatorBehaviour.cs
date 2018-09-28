a2a0cb4d0abd99e44eff3e1d29022c482d50c4b1
Jason Hess <jasonehess@hotmail.com>
2018-09-28 11:31:55 +1000

    elaborate on the null conditional operator

--- CSharpLang60/NullConditionalOperatorBehaviour.cs
+++ CSharpLang60/NullConditionalOperatorBehaviour.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
+using System;
 
 namespace CSharpLang60
 {
@@ -13,19 +14,64 @@ namespace CSharpLang60
         [Test]
         [TestCase(null, null)]
         [TestCase("bob", 3)]
-        public void ShouldReturnNullWhenOperatingOnNull(string underTest, int expectedLength)
+        public void ShouldReturnNullWhenOperatingOnNull(string stringUnderTest, int expectedValueOfNullConditional)
         {
             // If `underTest` is `null`, then `?.` should short-circuit and return null
-            underTest?.Length.Should().Be(expectedLength);
+            stringUnderTest?.Length.Should().Be(expectedValueOfNullConditional);
         }
 
         [Test]
         [TestCase(null, null)]
         [TestCase("bob", 3)]
-        public void ShouldReturnTypeOfFullExpressionEvenWhenNull(string underTest, int expectedLength)
+        public void ShouldReturnTypeOfTheRightHandSideOfExpressionEvenWhenNull(string stringUnderTest, int expectedLength)
         {
             // If `underTest` is `null`, then `?.` should short-circuit and return null
-            underTest?.Length.GetType().Should().Be(typeof(int));
+            stringUnderTest?.Length.GetType().Should().Be(typeof(int));
+        }
+
+        [Test]
+        public void IsUsefulToSetDefaultValues()
+        {
+            var person = new Person { FirstName = null };
+            person = null;
+            var defaultName = person?.FirstName ?? "Unknown";
+        }
+
+        [Test]
+        public void NullConditionalCanInvokeMembers()
+        {
+            Person person = null;
+            var personAsString = person?.ToString();
+            personAsString.Should().BeNull();
+        }
+
+        [Test]
+        public void NullConditionalIsUsefulForEventHandlersAndDelegates()
+        {
+            Person person = new Person();
+
+            // in versions of C# prior to 6.0 you might write something like:
+
+            if(person != null)
+            {
+                // store action as a temp variable to prevent race condition
+                // if person is accessible from elsewhere
+                var action = person.PerformAction;
+                if(action != null)
+                {
+                    action();
+                }
+            }
+
+            // This is what is preferred in C# 6:
+            person?.PerformAction?.Invoke();
+        }
+
+        private class Person
+        {
+            public string FirstName { get; set; }
+
+            public Action PerformAction = null;
         }
     }
 }
