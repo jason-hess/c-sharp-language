e829f2b72d29d096bd2cff45c80c7e188728d10a
Jason Hess <jasonehess@hotmail.com>
2018-09-28 11:42:59 +1000

    can initialize index style collections

---
+++ CSharpLang60/IndexInitializerBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace CSharpLang60
+{
+    public class IndexInitializerBehaviour
+    {
+        [Test]
+        public void InPreviousVersionsYouCouldInitialiseSequenceStyleCollections()
+        {
+            var list = new List<string>()
+            {
+                "one", "two", "three"
+            };
+        }
+
+        [Test]
+        public void CanNowInitializeIndexedCollections()
+        {
+            var dictionary = new Dictionary<int, string>
+            {
+                [1] = "one",
+                [2] = "two",
+                [3] = "three",
+            };
+        }
+    }
+}
