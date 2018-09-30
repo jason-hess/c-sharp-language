6a4bb7dde2b3cedecd205bf104517b19f9c5968e
Jason Hess <jasonehess@hotmail.com>
2018-09-30 15:40:22 +1000

    refactor

--- CSharpLang60/ExtensionAddMethodsForCollectionInitialisers.cs
+++ CSharpLang60/ExtensionAddMethodsForCollectionInitialisers.cs
@@ -6,6 +6,13 @@ using System.Linq;
 
 namespace CSharpLang60
 {
+    /// <summary>
+    /// C# 6 adds the ability to add collection initializer support to classes that
+    /// don't have an Add method but that you wish to use a collection initializer with.  
+    /// For example, if you have a class called People that is defined elsewhere
+    /// then you cann define an Add extension method in your own code to allow you
+    /// to use an extension initializer.
+    /// </summary>
     public class ExtensionAddMethodsForCollectionInitialisersBehaviour
     {
         [Test]
