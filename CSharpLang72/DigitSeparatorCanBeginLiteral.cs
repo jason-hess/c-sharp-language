61cf923475c842ff6565f97a70f31517aa330c58
Jason Hess <jasonehess@hotmail.com>
2018-10-04 12:16:00 +1000

    add some C# 7.2 features

---
+++ CSharpLang72/DigitSeparatorCanBeginLiteral.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace CSharpLang72
+{
+    /// <summary>
+    /// Before C# 7.2 the digit separator within a numeric literal needed to be between two numeric values (e.g. 10_000).
+    /// In C# 7.2 you can now start a numeric literal with a digit separator.
+    /// </summary>
+    public class DigitSeparatorCanBeginLiteral
+    {
+        [Test]
+        public void Should()
+        {
+            const int numericLiteralStartingWithDigitSeparator = 0b_10_10_10;
+            numericLiteralStartingWithDigitSeparator
+                .Should()
+                .Be(0b_10_10_10);
+        }
+    }
+
+    // Span is a window onto a whole or part of an array
+
+}
