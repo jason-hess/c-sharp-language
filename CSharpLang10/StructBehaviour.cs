b9e4a7543c1f53747ebf1f22db97b78a55fb6799
Jason Hess <jasonehess@hotmail.com>
2018-10-10 20:08:13 +1000

    add struct behaviour

--- CSharpLang10/StructBehaviour.cs
+++ CSharpLang10/StructBehaviour.cs
@@ -1,18 +1,20 @@
-﻿using System;
-using FluentAssertions;
+﻿using FluentAssertions;
 using NUnit.Framework;
 
 namespace CSharpLang10
 {
     /// <summary>
-    /// In C# 1.0
+    /// A struct is a value type. When a struct is created, the variable to which the struct
+    /// is assigned holds the struct's actual data. When the struct is assigned to a new variable,
+    /// it is copied. The new variable and the original variable therefore contain two separate
+    /// copies of the same data. Changes made to one copy do not affect the other copy.
     ///
     /// See: https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/structs
     /// </summary>
     public class StructBehaviour
     {
         [Test]
-        public void FieldsOfStructsAreInitializedToDefaultValuesWhenNewIsNotCalled()
+        public void FieldsOfStructsAreInitializedToDefaultValuesWhenDefaultNewIsCalled()
         {
             Rectangle r = new Rectangle(); // initializes all members to default.  This must be called before you access
             // if you don't initialize a member
@@ -35,10 +37,23 @@ namespace CSharpLang10
             r2.Length.Should().Be(9);
             r.Length.Should().Be(10);
         }
+
+        [Test]
+        public void NonDefaultConstructorsCanBeDefined()
+        {
+            Rectangle r = new Rectangle(10, 12);
+            r.Width.Should().Be(10);
+        }
     }
 
     public struct Rectangle
     {
+        public Rectangle(int width, int length)
+        {
+            Width = width;
+            Length = length;
+        }
+
         // structs cannot have a default constructor or a finalizer
         public int Width;
         public int Length;
