a7404ab88b057d9af04758b7d4e894bec25e7472
Jason Hess <Jason.Hess@michaelhill.com.au>
2018-10-29 21:31:24 +1000

    elaborate null coalescing operator

--- CSharpLang20/NullCoalescingOperatorBehaviour.cs
+++ CSharpLang20/NullCoalescingOperatorBehaviour.cs
@@ -1,7 +1,42 @@
-﻿namespace CSharpLang20
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace CSharpLang20
 {
+    /// <summary>
+    /// The null-coalescing operator <code>??</code> returns the left-hand operand
+    /// if the operand is not null, otherwise the right-hand operand is returned.
+    /// </summary>
     public class NullCoalescingOperatorBehaviour
     {
+        [Test]
+        public void ReturnsRightHandSideIfLeftHandOperandIsNull()
+        {
+            int? leftHandValue = 10;
+            int finalValue = leftHandValue ?? 11;
+        }
+        [Test]
+        public void LeftHandOperandMustBeNullable()
+        {
+            int? leftHandValue = 10;
+            int rightHandValue = 11;
+            int finalValue = leftHandValue ?? rightHandValue;
+            // finalValue = rightHandValue ?? 11; // Error CS0019  Operator '??' cannot be applied to operands of type 'int' and 'int' 
+        }
 
+        [Test]
+        public void Should()
+        {
+            string value = null;
+            AssertionExtensions.Should(value ?? "").Be("");
+        }
+
+        [Test]
+        public void TheOperandsMustBeSubsetsOfTheNullableType()
+        {
+            // the result of the operation is a non-nullable type
+            int? x = 10;
+            // int? y = x ?? ""; // CS0019  Operator '??' cannot be applied to operands of type 'int?' and 'string' 
+        }
     }
 }
