24301a1b4c2009838384546e303d9f451f7f8937
Jason Hess <Jason.Hess@michaelhill.com.au>
2018-10-29 21:37:39 +1000

    elaborate nullable types

--- CSharpLang20/NullableTypeBehaviour.cs
+++ CSharpLang20/NullableTypeBehaviour.cs
@@ -1,7 +1,17 @@
-﻿namespace CSharpLang20
+﻿using NUnit.Framework;
+
+namespace CSharpLang20
 {
+    /// <summary>
+    /// C# 2.0 introduced the System.Nullable which was a generic type that had
+    /// operators overloaded.
+    /// </summary>
     public class NullableTypeBehaviour
     {
+        [Test]
+        public void Could()
+        {
 
+        }
     }
 }
