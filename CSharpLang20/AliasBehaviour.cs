648f2b53d13f684cff41b9411ecac6a646ddc6ee
Jason Hess <jasonehess@hotmail.com>
2018-11-04 07:33:28 +1000

    add using alias details

--- CSharpLang20/AliasBehaviour.cs
+++ CSharpLang20/AliasBehaviour.cs
@@ -1,13 +1,28 @@
-﻿using x = System;
+﻿using FluentAssertions;
+using x = System;
 
 namespace CSharpLang20
 {
-    class AliasBehaviour
+    /// <summary>
+    /// C# 2.0 introduced aliases.  <code>using anAlias = namespace;</code> allowed you
+    /// to refer to the namespace with the alias.
+    /// </summary>
+    public class AliasBehaviour
     {
-        public void Method()
+        /// <summary>
+        /// The <code>global</code> namespace allows you to reference the root namespace
+        /// </summary>
+        public void GlobalNamespaceShouldReferToRootNamespace()
         {
             global::System.String x = "";
+            x.Should().Be("");
+        }
+
+        public void AliasRefersToNamespace()
+        {
+            // The `x` now refers to `System` because of the using alias at the top of the file.
             x::String y = "";
+            y.Should().Be("");
         }
     }
 }
