ec71cdfcbdb91a38413de95f36a9355d2fbb2a84
Jason Hess <jasonehess@hotmail.com>
2018-11-04 07:39:09 +1000

    using alias

--- CSharpLang20/AliasBehaviour.cs
+++ CSharpLang20/AliasBehaviour.cs
@@ -5,7 +5,7 @@ namespace CSharpLang20
 {
     /// <summary>
     /// C# 2.0 introduced aliases.  <code>using anAlias = namespace;</code> allowed you
-    /// to refer to the namespace with the alias.
+    /// to refer to the namespace with the alias using the namespace alias qualifier operator ::
     /// </summary>
     public class AliasBehaviour
     {
@@ -14,6 +14,8 @@ namespace CSharpLang20
         /// </summary>
         public void GlobalNamespaceShouldReferToRootNamespace()
         {
+            // The namespace qualifier operator :: needs to be placed between a using alias
+            // on the LHS and a member of the namespace on the RHS.
             global::System.String x = "";
             x.Should().Be("");
         }
