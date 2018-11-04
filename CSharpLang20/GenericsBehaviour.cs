e3b73c02e7e3bfa2c9cd527900b3e44d3f022080
Jason Hess <jasonehess@hotmail.com>
2018-11-04 22:06:21 +1000

    refactor

---
+++ CSharpLang20/GenericsBehaviour.cs
@@ -0,0 +1,12 @@
+﻿namespace CSharpLang20
+{
+    /// <summary>
+    /// http://www.yoda.arachsys.com/csharp/csharp2/delegates.html
+    ///
+    /// https://docs.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-version-history
+    /// </summary> 
+    public class GenericsBehaviour
+    {
+
+    }
+}
