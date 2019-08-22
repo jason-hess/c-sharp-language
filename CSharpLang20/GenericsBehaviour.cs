d55b3d21fb70fff88bf681648f00195912e27b19
Jason Hess <Jason.Hess@michaelhill.com.au>
2019-08-22 22:06:06 +1000

    adding generics behaviours

--- CSharpLang20/GenericsBehaviour.cs
+++ CSharpLang20/GenericsBehaviour.cs
@@ -14,6 +14,11 @@ namespace CSharpLang20
         // Generic classes and methods combine reusability, type safety and efficiency in a way that
         // their non-generic counterparts cannot. Generics are most frequently used with collections
         // and the methods that operate on them. 
+
+        // a type parameter is a placeholder for a specific type that a client specifies
+        // when they create an instance of the generic type
+
+        // A generic class cannot be used as is.  It is more like a blueprint of a Type.
         public class GenericContainer<T> 
         {
             private readonly T _containedInstance;
@@ -35,8 +40,19 @@ namespace CSharpLang20
         [Test]
         public void CanInstantiateClassWithVariousTypes()
         {
+            // Generic types allow the client to specify the type when the class
+            // is instantiated
             GenericContainer<int> container = new GenericContainer<int>(10);
             container.Value.Should().Be(10);
         }
+
+        // Generic Type constraints:
+        // where T : struct
+        // where T : class
+        // where T : unmanaged
+        // where T : new()
+        // where T : BaseClass
+        // where T : Interface
+        // where TOne : TTwo
     }
 }
