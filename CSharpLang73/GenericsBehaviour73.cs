bc94089105f4c067e73d72ed32a711db88682926
Jason Hess <Jason.Hess@michaelhill.com.au>
2019-08-25 12:20:21 +1000

    refactor

--- CSharpLang73/GenericsBehaviour73.cs
+++ CSharpLang73/GenericsBehaviour73.cs
@@ -43,44 +43,7 @@ namespace CSharpLang73
             // var intType = new EnumGenericTypeConstraint<int>(); // Error CS0315  The type 'int' cannot be used as type parameter 'T' in the generic type or method 'GenericsBehaviour73.EnumGenericTypeConstraint<T>'.There is no boxing conversion from 'int' to 'System.Enum'
             flagType.NotValues
                 .Should()
-                .BeEquivalentTo(new [] {Flag.Canadian});
+                .BeEquivalentTo(new[] {Flag.Canadian});
         }
-
-        /* C#
-
-Copy
-public static Dictionary<int, string> EnumNamedValues<T>() where T : System.Enum
-{
-    var result = new Dictionary<int, string>();
-    var values = Enum.GetValues(typeof(T));
-
-    foreach (int item in values)
-        result.Add(item, Enum.GetName(typeof(T), item));
-    return result;
-}
-The methods used make use of reflection, which has performance implications. You can call this method to build a collection that is cached and reused rather than repeating the calls that require reflection.
-
-You could use it as shown in the following sample to create an enum and build a dictionary of its values and names:
-
-C#
-
-Copy
-enum Rainbow
-{
-    Red,
-    Orange,
-    Yellow,
-    Green,
-    Blue,
-    Indigo,
-    Violet
-}
-C#
-
-Copy
-var map = EnumNamedValues<Rainbow>();
-
-foreach (var pair in map)
-    Console.WriteLine($"{pair.Key}:\t{pair.Value}"); */
     }
 }
