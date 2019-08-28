73cb2f5bb8108db8a781ce7b5b94177c80adc0d4
Jason Hess <Jason.Hess@michaelhill.com.au>
2019-08-28 11:50:37 +1000

    add custom parameters

--- CSharpLang10/AttributeBehaviour.cs
+++ CSharpLang10/AttributeBehaviour.cs
@@ -102,5 +102,54 @@ namespace CSharpLang10
             // - Controlling optimizations by the just-in-time (JIT) compiler so the code remains easy to debug.
             // - Obtaining information about the caller to a method.
         }
+
+        public class DefiningCustomAttributes
+        {
+            /// <summary>
+            /// To define a custom attribute you simply extend System.Attribute
+            /// </summary>
+            public class MyCustomAttribute : Attribute { }
+
+            [MyCustom]
+            public void UsageOfMyCustomAttribute() { }
+
+            /// <summary>
+            /// To define where your attribute is valid, decorate it with the AttributeUsage
+            /// attribute.
+            /// </summary>
+            [AttributeUsage(System.AttributeTargets.Method | System.AttributeTargets.Property)]
+            public class MyTargetedCustomAttribute : Attribute
+            {
+                // [MyTargetedCustom] // Error CS0592  Attribute 'MyTargetedCustom' is not valid on this declaration type. It is only valid on 'method, property, indexer' declarations.
+                private int _field = 0;
+            }
+
+            [AttributeUsage(System.AttributeTargets.All, AllowMultiple = true)]
+            public class MyMultipleAttribute : Attribute
+            {
+                [MyMultiple]
+                [MyMultiple]
+                public void Method() { }
+            }
+
+            public class AnyPublicReadWriteFieldsOrPropertiesAreParameters
+            {
+                public class MyAttributeWithParametersAttribute : Attribute
+                {
+                    public int FieldOne;
+                    private int _backingValue;
+
+                    public int PropertyOne
+                    {
+                        get { return _backingValue; }
+                        set { _backingValue = value; }
+                    }
+                }
+
+                [MyAttributeWithParameters(FieldOne = 1)]
+                public void Method() { }
+            }
+
+        }
     }
 }
