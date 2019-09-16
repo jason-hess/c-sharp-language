628098e51b651d78b373e60a3afda7910b37790d
Jason Hess <Jason.Hess@michaelhill.com.au>
2019-09-16 14:29:46 +1000

    complete cs6

---
+++ CSharpLang60/DeterministicBuildOutputs.cs
@@ -0,0 +1,13 @@
+﻿namespace CSharpLang60
+{
+    /// <summary>
+    /// The -deterministic option instructs the compiler to produce a byte-for-byte identical output assembly for successive compilations of the same source files.
+    /// 
+    /// By default, every compilation produces unique output on each compilation. The compiler adds a timestamp, and a GUID generated from random numbers. You use this option if you want to compare the byte-for-byte output to ensure consistency across builds.
+    /// 
+    /// https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/compiler-options/deterministic-compiler-option
+    /// </summary>
+    class DeterministicBuildOutputs
+    {
+    }
+}
