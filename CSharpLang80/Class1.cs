45fce166cac9ba52ca62947bebd4cfac2efe5ecb
Jason Hess <jasonehess@hotmail.com>
2019-09-17 13:50:48 +1000

    start building out C# 8 features

---
+++ CSharpLang80/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpLang80
+{
+    public class Class1
+    {
+        public void Method()
+        {
+            using var stream = new System.IO.StreamWriter("WriteLines2.txt");
+
+
+        }
+    }
+}
