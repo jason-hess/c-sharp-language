4477ec62f4ecb9c3b1da5b9166a0f173f6b88ca0
Jason Hess <Jason.Hess@michaelhill.com.au>
2019-09-18 21:55:42 +1000

    add more readonly structs

---
+++ CSharpLang72/StructBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpLang72
+{
+    class StructBehaviour
+    {
+        /// <summary>
+        /// The readonly struct declaration, to indicate that a struct is immutable and should be passed as an in parameter to its member methods. Adding the readonly modifier to an existing struct declaration is a binary compatible change.
+        /// </summary>
+        public readonly struct ReadOnlyPoint
+        {
+            public ReadOnlyPoint(int x)
+            {
+                X = x;
+            }
+
+            public int X;
+        }
+    }
+}
