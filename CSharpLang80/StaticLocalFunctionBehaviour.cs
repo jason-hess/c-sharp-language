5057fc17424c39604f8f1b2d933773e01d1a242a
Jason Hess <Jason.Hess@michaelhill.com.au>
2019-09-19 14:34:16 +1000

    add static local functions

--- CSharpLang80/StaticLocalFunctionBehaviour.cs
+++ CSharpLang80/StaticLocalFunctionBehaviour.cs
@@ -1,11 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using NUnit.Framework;
 
 namespace CSharpLang80
 {
     class StaticLocalFunctionBehaviour
     {
-
+        /// <summary>
+        /// local functions can now be static
+        /// </summary>
+        [Test]
+        public void Should()
+        {
+            static int Add(int x, int y) => x + y;
+            int Substract(int x, int y) => x - y;
+            Add(1, 2);
+            Substract(2, 1);
+        }
     }
 }
