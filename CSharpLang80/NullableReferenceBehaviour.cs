6ee32b744d8bb2cfd80a6eb404945f97811ca0c1
Jason Hess <Jason.Hess@michaelhill.com.au>
2019-10-16 10:42:38 +1000

    testing nullability

--- CSharpLang80/NullableReferenceBehaviour.cs
+++ CSharpLang80/NullableReferenceBehaviour.cs
@@ -8,7 +8,7 @@ namespace CSharpLang80
     /// C# 8.0 introduces nullable and non-nullable reference types that allow you to specify
     /// when a reference is supposed to or not supposed to be null.  Non-nullable reference types
     /// must be assigned a value and cannot be assigned null.  Nullable references can be assigned
-    /// null and may only be dereferenced when the compiler can guarantee the value isn't null.'
+    /// null and may only be dereferenced when the compiler can guarantee the value isn't null.
     ///
     /// Don't use this feature to remove all null values from your code. Rather, you should declare
     /// your intent to the compiler and other developers that read your code. By declaring your
@@ -25,9 +25,10 @@ namespace CSharpLang80
     public class NullableReferenceBehaviour
     {
         [Test]
+        [TestCase(null)]
         public void Method(Person o)
         {
-            var result = o.Name;
+            var result = o?.Name;
             string nonNullableReferenceType = null;
             string? nullableReferenceType = null;
             // The compiler warns you if you are dereferencing a nullable reference that might be null
