6678fe614f0120e823c1cb11406299290171bb1a
Jason Hess <Jason.Hess@michaelhill.com.au>
2019-11-02 11:37:11 +1000

    add C# 6.0 switch on enum

--- CSharpLang60/SwitchExpressionBehaviour.cs
+++ CSharpLang60/SwitchExpressionBehaviour.cs
@@ -20,7 +20,7 @@ namespace CSharpLang60
             }
 
             var aRealThing = aThing is Thing;
-            if(aRealThing != null)
+            if (aRealThing != null)
             {
                 return;
             }
@@ -30,7 +30,7 @@ namespace CSharpLang60
         public void SwitchCanBeEmpty()
         {
             int x = 10;
-            switch(x)
+            switch (x)
             {
 
             }
@@ -44,14 +44,20 @@ namespace CSharpLang60
             switch (x)
             {
                 // case 1: // Error CS0163  Control cannot fall through from one case label('case 1:') to another
-                   // x = 10;
+                // x = 10;
                 // case 2: // Error CS8070  Control cannot fall out of switch from final case label('case 2:')
-                   // x = 13;
+                // x = 13;
+            }
+
+
+            switch (x)
+            {
+                // default: // Error CS8070  Control cannot fall out of switch from final case label('default:') 
             }
         }
 
         [Test]
-        public void SwitchMustBeOneOfShortIntLongBoolStringCharOrEnum()
+        public void SwitchMustBeOneOfIntegralLikeShortIntLongBoolStringCharOrEnum()
         {
             float x = 10;
             // switch(x) // Error CS0151 A switch expression or case label must be a bool, char, string, integral, enum, or corresponding nullable type in C# 6 and earlier.
@@ -62,12 +68,88 @@ namespace CSharpLang60
         [Test]
         public void SwitchSectionCanHaveTwoLabels()
         {
-            switch(10)
+            switch (10)
             {
                 case 1:
                 case 2:
                     var result = "1 or 2";
                     return; // good to specify here although not needed?
+            }
+        }
+
+        [Test]
+        public void OnlyOneCaseWillEverMatchInCSharp60()
+        {
+            int x = 10;
+            string word;
+            switch (x)
+            {
+                case 1:
+                    word = "One";
+                    break;
+                default:
+                    word = "Unknown";
+                    break;
+            }
+        }
+
+        [Test]
+        public void InCSharp60OnlyTheConstantPatternExists()
+        {
+            object x = 10;
+            //switch (x) // Error CS0151 A switch expression or case label must be a bool, char, string, integral, enum, or corresponding nullable type in C# 6 and earlier.
+            {
+                //case 1:
+                //case "10":
+            }
+        }
+
+        [Test]
+        public void InCSharp60OnlyTheConstantPatternExists2()
+        {
+            int x = 10;
+            switch (x)
+            {
+                case 1:
+                    break;
+                //case "10": // Error CS0029  Cannot implicitly convert type 'string' to 'int' 
+
+                    break;
+            }
+        }
+
+        [Test]
+        public void CanReferToConstants()
+        {
+            int x = 10;
+            const int SUPER_THING = 1;
+            switch (x)
+            {
+                case SUPER_THING:
+                    break;
+                    //case "10": // Error CS0029  Cannot implicitly convert type 'string' to 'int' 
+
+                    break;
+            }
+        }
+
+        public void CanSwitchOnEnum()
+        {
+            switch (DateTime.Now.DayOfWeek)
+            {
+                case DayOfWeek.Sunday:
+                case DayOfWeek.Saturday:
+                    Console.WriteLine("The weekend");
+                    break;
+                case DayOfWeek.Monday:
+                    Console.WriteLine("The first day of the work week.");
+                    break;
+                case DayOfWeek.Friday:
+                    Console.WriteLine("The last day of the work week.");
+                    break;
+                default:
+                    Console.WriteLine("The middle of the work week.");
+                    break;
             }
         }
 
