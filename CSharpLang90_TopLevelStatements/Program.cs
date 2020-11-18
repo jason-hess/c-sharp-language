401247dd7bd99fd48cf26ee6ff2b67d9e0ad2eb8
Jason Hess <Jason.Hess@michaelhill.com.au>
2020-11-18 17:21:53 +1000

    add top-level statements

---
+++ CSharpLang90_TopLevelStatements/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+//
+// Top-level statements automatically generate a Main() method for the assembly 
+// (with <OutputType>Exe</OutputType> the csproj).  
+// There can only be one file top-level statments.  These are useful for console applications.
+// 
+Console.WriteLine("Hello World");
+
+// args is defined as previously for Main methods
+if (args.Length > 0)
+{
+    Console.WriteLine(args[0]);
+}
