952ee4b023f000bc7cbbce52309b85f22cd5124e
Jason Hess <Jason.Hess@michaelhill.com.au>
2025-01-17 16:15:03 +1000

    Revert the build optimisation - Visual Studio Test Explorer does not find tests unless the project has a reference to the Test SDK

---
+++ CSharpLang130/ParamsCollections.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace CSharpLang130
+{
+
+    public class ParamsCollections
+    {
+        /// <summary>
+        /// Prior to C# 13, the `params` keyword only support single-dimensional arrays
+        /// </summary>
+        public static int Add(params int[] values)
+        {
+            int sum = 0;
+            foreach (int value in values)
+            {
+                sum += value;
+            }
+            return sum;
+        }
+
+        [Test]
+        public void ShouldAdd() => Add(1, 2, 3).Should().Be(6);
+    }
+}
