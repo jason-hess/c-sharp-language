15d86f17568a209ea3be967ba6b571c0432fb195
Jason Hess <Jason.Hess@michaelhill.com.au>
2025-01-18 11:51:04 +1000

    Add C# 13 params collections

--- CSharpLang130/ParamsCollections.cs
+++ CSharpLang130/ParamsCollections.cs
@@ -1,9 +1,15 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
+using System.Collections.Immutable;
 
 namespace CSharpLang130
 {
 
+    /// <summar>
+    /// `params` arguments now supports Collection types
+    /// </summary>
+    /// <see href="https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/method-parameters#params-modifier" />
+    /// <seealso href="https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-13#params-collections"/>
     public class ParamsCollections
     {
         /// <summary>
@@ -21,5 +27,42 @@ namespace CSharpLang130
 
         [Test]
         public void ShouldAdd() => Add(1, 2, 3).Should().Be(6);
+
+        /// <summary>
+        /// In C# 13, you can now use any of:
+        ///   - A single-dimensional array
+        ///   - `Span<T>` or `ReadOnlySpan<T>`
+        ///   - A type that is supported by a Collection expression
+        ///   - A type that implements `IEnumerable<T>`
+        ///   - One of the following interface types:
+        ///     - System.Collections.Generic.IEnumerable<T>
+        ///     - System.Collections.Generic.ICollection<T>
+        ///     - System.Collections.Generic.IReadOnlyCollection<T>
+        ///     - System.Collections.Generic.IList<T>
+        ///     - System.Collections.Generic.IReadOnlyList<T>
+        /// </summary>
+        public static int AddAsSpan(params ReadOnlySpan<int> values)
+        {
+            // The benefits here are
+            //   - that I now have access to all of the features of ReadOnlySpan<int>,
+            //     rather than converting from a single-dimensional array into a Span
+            //   - I can reuse other functions that may use a ReadOnlySpan rather
+            //     than a single-dimensional array
+            var newSpan = values.Slice(1);
+
+            int sum = values[0];
+            foreach (int value in newSpan)
+            {
+                sum += value;
+            }
+            return sum;
+        }
+
+        /// <summary>
+        /// The compiler will implement this as a conversion to a Span
+        /// and then call the method.
+        /// </summary>
+        [Test]
+        public void ShouldAddAsSpan() => AddAsSpan(1, 2, 3).Should().Be(6);
     }
 }
