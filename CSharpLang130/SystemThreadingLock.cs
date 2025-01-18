f91c4f16602130d61debe3e1016a32f5c27f163a
Jason Hess <Jason.Hess@michaelhill.com.au>
2025-01-18 13:30:49 +1000

    Elaborate System.Threading.Lock C# 13 Feature

--- CSharpLang130/SystemThreadingLock.cs
+++ CSharpLang130/SystemThreadingLock.cs
@@ -6,6 +6,7 @@ namespace CSharpLang130
     /// C# 13 introduces the System.Threading.Lock class which offers better performance
     /// than the System.Threading.Monitor.
     /// </summary>
+    /// <see href="https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-13#new-lock-object"/>
     public class SystemThreadingLock
     {
         /// <summary>
@@ -34,19 +35,68 @@ namespace CSharpLang130
         public void ShouldUseMonitor()
         {
             var o = new object();
-            lock(o)
+            lock(o) // The Compiler will transpile this into a System.Threading.Monitor call
             {
-                // Should only be one thread allowed in here at a time
+                // Thread-Safe execution here
             }
+
+            // You can also use System.Threading.Monitor directly - but its
+            // easiest to just let the compiler do the work for you.
         }
 
         /// <summary>
-        /// The new System.Threading.Lock
+        /// The new System.Threading::Lock returns a System.Threading::Lock.Scope 
+        /// - a `ref struct` that implements Disposable.
+        /// 
+        /// When the `lock` keyword is used with a variable of type System.Threading::Lock
+        /// then the compiler will transpile to use System.Threading.Lock.
         /// </summary>
         [Test]
         public void ShouldUseLock()
         {
+            Lock aLock = new();
+            lock (aLock) {
+                Console.WriteLine("thread safe here");
+            }
 
+            // the above transpiles to:
+
+            Lock @lock = new Lock();
+            Lock.Scope scope = @lock.EnterScope();
+            try
+            {
+                Console.WriteLine("thread safe here");
+            }
+            finally
+            {
+                scope.Dispose();
+
+            }
+
+            // If the variable is not a System.THreading.Lock, (even if the variable
+            // references a type of System.Threading.Lock, then the compiler will 
+            // revert to using System.Threading::Monitor).
+            object o = new Lock(); // CS9216: A value of type System.Threading.Lock converted to a different type will use likely unintended monitor-based locking in lock statement.
+            lock (o)
+            {
+                Console.WriteLine("thread safe here with a Monitor");
+            }
+
+            // The above transpiles to:
+            object obj = new Lock(); // 
+            bool lockTaken = false;
+            try
+            {
+                Monitor.Enter(obj, ref lockTaken);
+                Console.WriteLine("thread safe here with a Monitor");
+            }
+            finally
+            {
+                if (lockTaken)
+                {
+                    Monitor.Exit(obj);
+                }
+            }
         }
     }
 }
