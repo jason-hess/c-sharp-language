749701b509bcf06379b4f6144cde916b4d4398b8
Jason Hess <Jason.Hess@michaelhill.com.au>
2025-01-22 16:11:50 +1000

    Add New Escape Sequence C# 13 Feature

---
+++ CSharpLang130/NewEscapeSequence.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace CSharpLang130
+{
+    /// <summary>
+    /// You can use \e as a character literal escape sequence for the ESCAPE character, Unicode U+001B. 
+    /// Previously, you used \u001b or \x1b. Using \x1b wasn't recommended because if the next characters 
+    /// following 1b were valid hexadecimal digits, those characters became part of the escape sequence.
+    /// </summary>
+    internal class NewEscapeSequence
+    {
+        [Test]
+        public void ShouldEscape()
+        {
+            '\e'.Should().Be('\x1b');   
+        }
+    }
+}
