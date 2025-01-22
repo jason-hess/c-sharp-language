878c433f82ad4fab6b4d7155cfc68a570fe66a1b
Jason Hess <Jason.Hess@michaelhill.com.au>
2025-01-22 15:11:02 +1000

    Performance Test System.Threading.Lock C# 13 Feature

--- CSharpLang130_Performance/Program.cs
+++ CSharpLang130_Performance/Program.cs
@@ -1,41 +1,4 @@
-﻿using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Running;
+using CSharpLang130_Performance;
 
 var _ = BenchmarkRunner.Run<SystemThreadingLockPerformance>();
-
-[MemoryDiagnoser]
-public class SystemThreadingLockPerformance
-{
-    readonly object _legacyLock = new();
-    readonly Lock _lock = new();
-    readonly object _lockLock;
-
-    public SystemThreadingLockPerformance()
-    {
-        _lockLock = _lock;
-    }
-
-    [Benchmark]
-    public void LegacyLock()
-    {
-        lock (_legacyLock)
-        {
-        }
-    }
-
-    [Benchmark]
-    public void Lock()
-    {
-        lock (_lock)
-        {
-        }
-    }
-
-    [Benchmark]
-    public void MisuseOfLock()
-    {
-        lock (_lockLock)
-        {
-        }
-    }
-}
---
+++ CSharpLang130_Performance/SystemThreadingLockPerformance.cs
@@ -0,0 +1,45 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace CSharpLang130_Performance;
+
+[MemoryDiagnoser]
+public class SystemThreadingLockPerformance
+{
+    readonly object _legacyLock = new();
+    readonly Lock _lock = new();
+    readonly object _lockLock;
+
+    public SystemThreadingLockPerformance()
+    {
+        // Disable the warning since we are intending to test performance
+        // of converting to a monitor-based lock
+
+#pragma warning disable CS9216 // A value of type 'System.Threading.Lock' converted to a different type will use likely unintended monitor-based locking in 'lock' statement.
+        _lockLock = _lock;
+#pragma warning restore CS9216 // A value of type 'System.Threading.Lock' converted to a different type will use likely unintended monitor-based locking in 'lock' statement.
+    }
+
+    [Benchmark]
+    public void LegacyLock()
+    {
+        lock (_legacyLock)
+        {
+        }
+    }
+
+    [Benchmark]
+    public void Lock()
+    {
+        lock (_lock)
+        {
+        }
+    }
+
+    [Benchmark]
+    public void MisuseOfLock()
+    {
+        lock (_lockLock)
+        {
+        }
+    }
+}
