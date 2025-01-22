d74a4abcb4562aa1783a8b198f5ee17706f42982
Jason Hess <Jason.Hess@michaelhill.com.au>
2025-01-22 16:25:22 +1000

    Add Index Access from End C# 13 Feature

--- CSharpLang130/IndexAccessInObjectInitializer.cs
+++ CSharpLang130/IndexAccessInObjectInitializer.cs
@@ -1,6 +1,73 @@
-﻿namespace CSharpLang130
+﻿using NUnit.Framework;
+
+namespace CSharpLang130;
+
+/// <summary>
+/// 
+/// </summary>
+/// <see href="https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-13#implicit-index-access" />
+public class IndexAccessInObjectInitializer
 {
-    internal class IndexAccessInObjectInitializer
+    public class Initilizable
     {
+        public int[] Value = new int[10];
     }
+
+    /// <summary>
+    /// The implicit "from the end" index operator, ^, is now allowed in an object initializer expression.
+    /// </summary>
+    [Test]
+    public void Should()
+    {
+        _ = new Initilizable()
+        {
+            Value =
+            {
+                [^1] = 0,
+                [^2] = 1,
+                [^3] = 2,
+                [^4] = 3,
+                [^5] = 4,
+                [^6] = 5,
+                [^7] = 6,
+                [^8] = 7,
+                [^9] = 8,
+                [^10] = 9
+            }
+        };
+
+
+        // The above transpiles to the following:
+        //     public class Initilizable
+        //{
+        //    [Nullable(1)]
+        //public int[] Value = new int[10];
+        //}
+
+        //public void Should()
+        //{
+        //    Initilizable initilizable = new Initilizable();
+        //    int num = initilizable.Value.Length - 1;
+        //    initilizable.Value[num] = 0;
+        //    int num2 = initilizable.Value.Length - 2;
+        //    initilizable.Value[num2] = 1;
+        //    int num3 = initilizable.Value.Length - 3;
+        //    initilizable.Value[num3] = 2;
+        //    int num4 = initilizable.Value.Length - 4;
+        //    initilizable.Value[num4] = 3;
+        //    int num5 = initilizable.Value.Length - 5;
+        //    initilizable.Value[num5] = 4;
+        //    int num6 = initilizable.Value.Length - 6;
+        //    initilizable.Value[num6] = 5;
+        //    int num7 = initilizable.Value.Length - 7;
+        //    initilizable.Value[num7] = 6;
+        //    int num8 = initilizable.Value.Length - 8;
+        //    initilizable.Value[num8] = 7;
+        //    int num9 = initilizable.Value.Length - 9;
+        //    initilizable.Value[num9] = 8;
+        //    int num10 = initilizable.Value.Length - 10;
+        //    initilizable.Value[num10] = 9;
+        //}
+    }
+
 }
