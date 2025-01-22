3605b5b161f43170f758a032d1f1681619704f1a
Jason Hess <Jason.Hess@michaelhill.com.au>
2025-01-22 16:13:33 +1000

    Add New Escape Sequence C# 13 Feature

--- CSharpLang130/NewEscapeSequence.cs
+++ CSharpLang130/NewEscapeSequence.cs
@@ -8,6 +8,7 @@ namespace CSharpLang130
     /// Previously, you used \u001b or \x1b. Using \x1b wasn't recommended because if the next characters 
     /// following 1b were valid hexadecimal digits, those characters became part of the escape sequence.
     /// </summary>
+    /// <see href="https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-13#new-escape-sequence" />
     internal class NewEscapeSequence
     {
         [Test]
