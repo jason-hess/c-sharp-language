3e5330aac3145e91c6da8f4cbcdb64137ac049ab
Jason Hess <Jason.Hess@michaelhill.com.au>
2025-01-23 17:01:24 +1000

    Refactor

--- CSharpLang130/NewEscapeSequence.cs
+++ CSharpLang130/NewEscapeSequence.cs
@@ -4,17 +4,15 @@ using NUnit.Framework;
 namespace CSharpLang130
 {
     /// <summary>
-    /// You can use \e as a character literal escape sequence for the ESCAPE character, Unicode U+001B. 
-    /// Previously, you used \u001b or \x1b. Using \x1b wasn't recommended because if the next characters 
+    /// You can use \e as a character literal escape sequence for the ESCAPE character (i.e. Unicode U+001B).
+    /// Previously, you used \u001b or \x1b. Using \x1b wasn't recommended because if the next characters
     /// following 1b were valid hexadecimal digits, those characters became part of the escape sequence.
     /// </summary>
     /// <see href="https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-13#new-escape-sequence" />
     internal class NewEscapeSequence
     {
         [Test]
-        public void ShouldEscape()
-        {
-            '\e'.Should().Be('\x1b');   
-        }
+        public void ShouldEscape() => 
+            '\e'.Should().Be('\x1b');
     }
 }
